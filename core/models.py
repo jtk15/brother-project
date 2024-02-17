595b4e9d80e92b153d00425221e9c2e48eb0a0b1
jtk15 <joeltonportopl15@gmail.com>
2024-02-16 21:01:43 -0300

    Foi implementado os modelos iniciais da aplicação.
    closes #2

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    
+    name = models.CharField('Nome', max_length=200)
+    
+    class Meta:
+        
+        abstract = True
+    
+    
+class Category(Base):
+       
+    def __str__(self):
+        return f'Categoria {self.name}'
+    
+    
+class Product(Base):
+ 
+    price = models.DecimalField('Preço', max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='categories', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'Produto {self.name}'
