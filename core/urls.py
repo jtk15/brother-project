22a05808ed0a473945f839def8ecff8bf5c6bf35
jtk15 <joeltonportopl15@gmail.com>
2024-02-24 03:30:06 -0300

    Foi feito melhoria no links do site, foi criado os layout de todas as páginas e linkados com sua rota.
    Tem algumas paginas que não estão finalizadas, como contato, login.
    
    related #24

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from core.views import index, products, products_by_category 
+from core.views import index, products, products_by_category, promotions, contact
 
 
 urlpatterns = [
-    path('index', index),
+    path('inicio', index, name='index'),
     path('produtos', products, name='products'),
     path('produtos/<str:slug>', products_by_category, name='products'),
+    path('promocoes', promotions, name='promotions'),
+    path('contatos', contact, name='contact'),
 ]
