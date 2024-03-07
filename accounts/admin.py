a6c085775df20a56a262b347977a917f4a9a21ec
jtk15 <joeltonportopl15@gmail.com>
2024-03-07 14:44:33 -0300

    Foi implementado o cadastro  de usuários
    closes  #28

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    name = forms.CharField(label='Nome', max_length=100)
+    surname = forms.CharField(label='Sobre Nome', max_length=100)
+    email = forms.EmailField(max_length=100)
+    phone = forms.CharField(label='Telefone', max_length=100)
+    code = forms.CharField(label='Cep', max_length=11, required=False)
+    address = forms.CharField(label='Endereço', max_length=200, required=False)
+    complement = forms.CharField(label='Complemento', max_length=200, required=False)
+    
+    
+    class Meta:
+        
+        model = User
+        fields = ('name', 'surname', 'username', 'email', 'phone', 'password1', 'password2', 'code', 'address', 'complement')
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm 
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .admin import CustomUserCreationForm
 
 # Create your views here.
 def register(request):  
+    user_empty_data = {}
+    user_data = {}
+    field_kyes = ['name', 'surname', 'username', 'email', 'phone', 'password1', 'password2'] #'code', 'address', 'complement']
     
-    if request.POST == 'POST':  
-        form = UserCreationForm()  
-        if form.is_valid():  
+    
+    if request.method == 'POST':
+        
+        datas = request.POST
+        
+        for key in datas.keys():
+            user_data.update({
+                key: datas[key]
+            })
+        
+        for key in field_kyes:
+            if datas[key] == '':
+                user_empty_data[key]=f'Campo Obrigatorio' 
+
+        form = CustomUserCreationForm(request.POST)
+    
+        if form.is_valid():
             form.save()
-            print('correto')  
-    else:          
-        print('error')
-        form = UserCreationForm()  
+            messages.success(request, 'Cadastro Realizado com Sucesso')
+            
+        else:
+            if request.method == 'POST':
+                messages.error(request, 'Formulário Inválido')
+            form = CustomUserCreationForm()  
+       
           
           
-    return render(request, 'registration/register.html')  
+    return render(request, 'registration/register.html', context={'user_empty_data':user_empty_data, 'user_data':user_data})  
